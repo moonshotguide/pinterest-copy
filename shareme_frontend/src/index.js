f10c4361e96c5648e6fe847c45683dd11ba926fd
David Castagneto A <davidcastagnetoa@gmail.com>
2022-11-04 09:01:22 +0100

    Google Login/Create User Button successfully

--- shareme_frontend/src/index.js
+++ shareme_frontend/src/index.js
@@ -11,3 +11,18 @@ ReactDOM.render(
     </Router>,
     document.getElementById('root')
 );
+
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import { BrowserRouter as Router } from "react-router-dom";
+
+// import App from "./App";
+// import "./index.css";
+
+// createRoot(
+//     document.getElementById("root")
+//     ).render(
+//     <Router>
+//         <App />
+//     </Router>
+// );
