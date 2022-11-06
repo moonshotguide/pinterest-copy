863af6ef063575fe84f0bdd92be24354f6cc0fb4
David Castagneto A <76570078+davidcastagnetoa@users.noreply.github.com>
2022-11-06 23:23:09 +0000

    localstorage

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
         <Link to="/">
           <img src={logo} alt="logo" className="w-28" />
         </Link>
-        <Link to={`user-profile/${user?._id}`}>
+        <Link to={`user-profile/${user?._sub}`}>
           <img src={user?.image} alt="logo" className="w-28" />
         </Link>
 
