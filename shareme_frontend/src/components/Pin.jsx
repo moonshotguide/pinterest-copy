0e31d3f9d448dc2d4a6fadae5ff0a5fe01c4886c
David Castagneto A <davidcastagnetoa@gmail.com>
2022-11-22 07:39:15 +0100

    img postedBy

--- shareme_frontend/src/components/Pin.jsx
+++ shareme_frontend/src/components/Pin.jsx
@@ -5,6 +5,7 @@ import { BsFillArrowUpRightCircleFill } from "react-icons/bs";
 import { AiTwotoneDelete } from "react-icons/ai";
 import { GiSave } from "react-icons/gi";
 import { GoDesktopDownload, GoPin } from "react-icons/go";
+import { MdDeleteForever } from "react-icons/md";
 import { client, urlFor } from "../client";
 import { fetchUser } from "../utils/fetchUser";
 
@@ -16,7 +17,8 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
 
   // let alreadySaved = !!(save?.filter((item) => item.postedBy.id === user?.sub));
   let alreadySaved = !!(save?.filter((item) => item?.postedBy?._id === user?.sub));
-  console.log(client.postedBy?._id);
+
+  console.log(save);
   console.log(user?.sub);
   console.log(alreadySaved);
 
@@ -61,7 +63,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
     <div className="m-2">
       <div
         onMouseEnter={() => setPostHovered(true)}
-        onMouseLeave={() => setPostHovered(false)}
+        onMouseLeave={() => setPostHovered(false)} // change to true to see the div in devtools
         onClick={() => navigate(`/pin-detail/${_id}`)}
         className="relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 easy-in-out"
       >
@@ -81,7 +83,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                   href={`${image?.asset?.url}?dl=`}
                   download
                   onClick={(e) => e.stopPropagation()}
-                  className="bg-slate-900 dark:bg-slate-900 w-fit h-8 rounded-full flex items-center justify-center text-md px-3 opacity-70 hover:opacity-100 hover:shadow-md outline-none text-white dark:text-gh_button_text hover:bg-gh_l_button_hover dark:hover:bg-gh_button_hover"
+                  className="bg-slate-900 dark:bg-slate-900 w-fit h-9 rounded-full flex items-center justify-center text-sm px-3 opacity-70 hover:opacity-100 hover:shadow-md outline-none text-white dark:text-gh_button_text hover:bg-gh_l_button_hover dark:hover:bg-gh_button_hover"
                 >
                   <GoDesktopDownload fontSize={18} className="mr-2" />
                   Download
@@ -90,9 +92,9 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
               {alreadySaved ? (
                 <button
                   type="button"
-                  className="bg-slate-700 dark:bg-slate-700 w-fit h-12 rounded-full flex items-center justify-center text-md px-5 opacity-80 hover:opacity-100 hover:shadow-md outline-none text-white dark:text-gh_button_text hover:bg-slate-900 dark:hover:bg-slate-900"
+                  className="bg-teal-700 dark:bg-teal-700 w-fit h-9 rounded-full flex items-center justify-center text-sm px-3 opacity-80 hover:opacity-100 hover:shadow-md outline-none text-white dark:text-gh_button_text hover:bg-teal-900 dark:hover:bg-teal-900"
                 >
-                  <GoPin className="mr-2" /> Saved {save?.lenght}
+                  <GoPin fontSize={18} className="mr-2" /> Saved {save?.lenght}
                 </button>
               ) : (
                 <button
@@ -101,9 +103,9 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                     savePin(_id);
                   }}
                   type="button"
-                  className="bg-red-700 dark:bg-red-700 w-fit h-12 rounded-full flex items-center justify-center text-md px-5 opacity-80 hover:opacity-100 hover:shadow-md outline-none text-white dark:text-gh_button_text hover:bg-red-900 dark:hover:bg-red-900"
+                  className="bg-red-700 dark:bg-red-700 w-fit h-9 rounded-full flex items-center justify-center text-sm px-3 opacity-80 hover:opacity-100 hover:shadow-md outline-none text-white dark:text-gh_button_text hover:bg-red-900 dark:hover:bg-red-900"
                 >
-                  <GiSave className="mr-2" /> Save
+                  <GiSave fontSize={18} className="mr-2" /> Save
                   {/* {savingPost? 'saving' : 'Save'} */}
                 </button>
               )}
@@ -114,7 +116,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                   href={destination}
                   target="_blank"
                   rel="noreferrer"
-                  className="bg-white flex items-center gap-2 text-black text-xs font-bold p-2 pl-4 pr-4 rounded-full opacity-80 hover:opacity-100 hover:shadow-md"
+                  className="bg-white flex items-center gap-2 text-black text-sm font-bold p-2 pl-4 pr-4 rounded-full opacity-80 hover:opacity-100 hover:shadow-md"
                 >
                   <BsFillArrowUpRightCircleFill />
                   {destination.length > 20 ? destination.slice(12, 24) : destination.slice(8)}
@@ -128,15 +130,29 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                     deletePin(_id);
                   }}
                   type="button"
-                  className="bg-red-700 dark:bg-red-700 w-fit h-12 rounded-full flex items-center justify-center text-md px-5 opacity-80 hover:opacity-100 hover:shadow-md outline-none text-white dark:text-gh_button_text hover:bg-red-900 dark:hover:bg-red-900"
+                  className="bg-slate-900 w-fit h-9 rounded-full flex items-center justify-center font-bold px-3 opacity-80 hover:opacity-100 hover:shadow-md outline-none text-white dark:text-gh_button_text hover:bg-black dark:hover:bg-black"
                 >
-                  <GiSave className="mr-2" /> Delete
+                  <MdDeleteForever fontSize={26} />
                 </button>
               )}
             </div>
           </div>
         )}
       </div>
+
+      <Link 
+        to={`user-profile/${user?._id}`}
+        className='flex gap-2 mt-2 items-center'
+      >
+        <img 
+          className="w-8 h-8 rounded-full object-cover border border-solid border-cyan-400"
+          src={postedBy?.image} 
+          alt="user-profile" 
+        />
+        <p className="text-sm">
+          {postedBy?.userName}
+        </p>
+      </Link>
     </div>
   );
 };
